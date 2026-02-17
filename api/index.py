7ea954cd101c5d56b055ce202d6533f87b102765
okapidragon <142844243+okapidragon@users.noreply.github.com>
2026-02-17 13:02:15 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -15,16 +15,16 @@ def home():
     def thing():
         global location
         response = (requests.get(f"https://api.weatherapi.com/v1/current.json?key=19e3ea7f765c4e42974134252252007&q={location}&aqi=yes")).json()
-            if action == "is_day":
-              if response["current"][action] == "1":
-                  output_message = "Yes"
-              elif response["current"][action] == "0":
-                  output_message = "No"
-            elif action == "condition":
-                output_message = response["current"]["condition"]["text"]
-            elif action == "co" or action == "no2" or action == "o3" or action == "so2" or action == "pm2_5" or action == "pm10" or action == "us-epa-index" or action == "gb-defra-index":
-                output_message = response["current"]["air_quality"][action]
-            else:
-                output_message = response["current"][action]
+          if action == "is_day":
+            if response["current"][action] == "1":
+                output_message = "Yes"
+            elif response["current"][action] == "0":
+                output_message = "No"
+          elif action == "condition":
+              output_message = response["current"]["condition"]["text"]
+          elif action == "co" or action == "no2" or action == "o3" or action == "so2" or action == "pm2_5" or action == "pm10" or action == "us-epa-index" or action == "gb-defra-index":
+              output_message = response["current"]["air_quality"][action]
+          else:
+              output_message = response["current"][action]
     thing()
   return render_template("index.html", result=output_message)
